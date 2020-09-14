4e3e72516fc4b9405fb35480a09496080787e66f
Julien Viet <julien@julienviet.com>
2020-09-14 14:09:31 +0200

    make work

--- src/main/java/codegen/test/service/SyncService.java
+++ src/main/java/codegen/test/service/SyncService.java
@@ -15,8 +15,6 @@ import java.time.ZonedDateTime;
 public interface SyncService{
 
 
-    void handle0(ZonedDateTime request , Handler<AsyncResult<Boolean>> handler);
-
-    void handle1(String request , Handler<AsyncResult<Boolean>> handler);
+    void handle(String request , Handler<AsyncResult<Boolean>> handler);
 
 }
